d18c82a7f4311a162a71271a3dbe3df5f00d4bad
N1kolayy <3878437@gmail.com>
2022-07-17 18:41:22 +0300

    changed the colors of the boxes through js

---
+++ index.js
@@ -0,0 +1,19 @@
+let grey = document.getElementById('grey');
+let red = document.getElementById('red');
+let black = document.getElementById('black');
+let yellow = document.getElementById('yellow');
+let blue = document.getElementById('blue');
+let pink = document.getElementById('pink');
+document.getElementById("green").style.backgroundColor = "green"
+document.getElementById("orange").style.backgroundColor = "orange"
+
+
+
+grey.style.backgroundColor = 'grey';
+red.style.backgroundColor = 'red';
+black.style.backgroundColor = 'black';
+yellow.style.backgroundColor = 'yellow';
+blue.style.backgroundColor = 'blue';
+pink.style.backgroundColor = 'pink';
+
+
